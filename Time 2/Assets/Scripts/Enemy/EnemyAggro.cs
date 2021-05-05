63d4a7d03991da64c3d87a4b03180305a30fd1b0
Rodrigo Malhães Bukowitz <rodrigomalhaes@gmail.com>
2021-05-04 23:26:37 -0300

    Inimigo Basico com Aggro

---
+++ Time 2/Assets/Scripts/Enemy/EnemyAggro.cs
@@ -0,0 +1,129 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAggro : MonoBehaviour
+{
+    public float aggroRadius;
+    public float aggroSpeed;
+    public float aggroPreparationTime;
+
+    public float resetAggroTime;
+
+    private EnemyMovement _enemyMovement;
+    private Transform _transform;
+    private Transform _playerTransform;
+
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+
+    private bool _performingAggro = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _enemyMovement = gameObject.GetComponent<EnemyMovement>();
+        _transform = transform;
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        _sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (PlayerInRange())
+        {
+            _enemyMovement.enemyState = EnemyState.Aggro;
+            PerformAggro();
+        }
+    }
+
+    private bool PlayerInRange()
+    {
+        LayerMask layer = LayerMask.GetMask( "Player");
+        Collider2D[] hitWall = Physics2D.OverlapCircleAll(_transform.position, aggroRadius, layer);
+        
+        // Verificar se o player esta no range 
+        if (hitWall.Length > 0)
+        {
+            // Pegar referencia da posição do player, player sempre sera o primeiro e E unico collider
+            if (!_performingAggro)
+            {
+                _playerTransform = hitWall[0].transform;
+            }
+            if(_playerTransform.position.y < _transform.position.y + _sr.bounds.size.y*0.5f)
+                return true; 
+        }
+        
+        return false;
+        
+    }
+
+    private void PerformAggro()
+    {
+        if (!_performingAggro)
+        {
+            _performingAggro = true;
+            float direction = 1f;
+            if (_playerTransform.position.x < transform.position.x)
+            {
+                direction = -1f;
+            }
+            _rb.velocity = Vector2.zero;
+            bool isFlipped = _enemyMovement.isFlipped;
+             if (!isFlipped && direction > 0)
+             {
+                 Vector3 newLocalScale = transform.localScale;
+                 newLocalScale.x *= -1;
+                 _transform.localScale = newLocalScale;
+                 _enemyMovement.isFlipped = true;
+             }
+            
+             // movendo para a direta flipado. Vai flipar 
+             if (isFlipped && direction < 0)
+             {
+                 Vector3 newLocalScale = transform.localScale;
+                 newLocalScale.x *= -1;
+                 _transform.localScale = newLocalScale;
+                 _enemyMovement.isFlipped = false;
+             }
+            
+            Vector2 MovePos = new Vector2(
+                    direction * aggroSpeed, //MoveTowards on 1 axis
+                    0f
+                );
+            // _transform.position = MovePos;
+            
+            // _rb.velocity = MovePos;
+            StartCoroutine(PrepareAggro(MovePos));
+        }
+
+    }
+    
+    private IEnumerator StopAggro()
+    {
+        yield return new WaitForSeconds(resetAggroTime);
+        _performingAggro = false;
+        _enemyMovement.enemyState = EnemyState.Idle;
+    }
+    
+    private IEnumerator PrepareAggro(Vector2 MovePos)
+    {
+        AudioManager.instance.Play("Tombo");
+        
+        yield return new WaitForSeconds(aggroPreparationTime);
+        _rb.AddForce(MovePos, ForceMode2D.Impulse);
+        StartCoroutine(StopAggro());
+        
+
+    }
+    
+    
+    // Funcao para debugar as hitboxes 
+    // private void OnDrawGizmosSelected()
+    // {       
+    //         Vector3 position = _transform.position;
+    //         Vector2 aggroPosition = new Vector2(position.x, position.y);
+    //
+    //         Gizmos.DrawWireSphere(aggroPosition, aggroRadius);
+    // }
+}
--- Time 2/Assets/Scripts/Enemy/EnemyMovement.cs
+++ Time 2/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -18,14 +18,26 @@ public class EnemyMovement : MonoBehaviour
     
     //state of the enemy 
     [HideInInspector] public EnemyState enemyState = EnemyState.Idle;
+    private bool going;
 
+    private Rigidbody2D _rb;
+
+    private void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+        going = false;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Flip();
         if(enemyMovement == SimpleEnemyMovements.Horizontal && enemyState == EnemyState.Idle)
         {
             HorizontalMovement();
+        }else if (enemyMovement == SimpleEnemyMovements.Vertical && enemyState == EnemyState.Idle)
+        {
+            VerticalMovement();
         }
     }
 
@@ -36,30 +48,94 @@ public class EnemyMovement : MonoBehaviour
         Vector3 pointToStart = new Vector3(pointStart.position.x, transform.position.y, transform.position.z);
         Vector3 pointToEnd = new Vector3(pointEnd.position.x, transform.position.y, transform.position.z);
 
-        transform.position = Vector3.Lerp(pointToStart, pointToEnd, Mathf.PingPong(Time.time * speed, 1.0f));
+        //Vector3 idleDestination = Vector3.Lerp(pointToStart, pointToEnd, Mathf.PingPong(Time.time * speed, 1.0f));
+        float direction = 0f;
+        if (going)
+        {
+            direction = Mathf.Sign(pointToStart.x - transform.position.x);
+        }
+        else
+        {
+            direction = Mathf.Sign(pointToEnd.x - transform.position.x);
+        }
+        
+        if (transform.position.x < pointToStart.x + Mathf.Abs(pointToEnd.x - pointToStart.x)/10)
+        {
+            going = false;
+        }
+        else if (transform.position.x > pointToEnd.x - Mathf.Abs(pointToEnd.x - pointToStart.x)/10)
+        {
+            going = true;
+        }
+        
+        Vector2 MovePos = new Vector2(direction  * speed * Time.deltaTime, //MoveTowards on 1 axis
+            0
+        );
+        //transform.position = MovePos;
+        
+        _rb.velocity = MovePos;
+    }
+    
+    private void VerticalMovement()
+    {
+        Vector3 pointToStart = new Vector3(transform.position.x, pointStart.position.y, transform.position.z);
+        Vector3 pointToEnd = new Vector3(transform.position.x, pointEnd.position.y, transform.position.z);
+
+        Vector3 idleDestination = Vector3.Lerp(pointToStart, pointToEnd, Mathf.PingPong(Time.time * speed, 1.0f));
+        float direction = Mathf.Sign(idleDestination.y - transform.position.y);
+        Vector2 MovePos = new Vector2(
+            transform.position.x, //MoveTowards on 1 axis
+            transform.position.y + direction
+        );
+        transform.position = MovePos;
     }
 
+    // private void Flip()
+    // {
+    //     Vector3 pos = transform.position;
+    //     // movendo para a esquerda não flipado. Vai flipar 
+    //     if (!isFlipped && pos.x == pointEnd.position.x )
+    //     {
+    //         Vector3 newLocalScale = transform.localScale;
+    //         newLocalScale.x *= -1;
+    //         transform.localScale = newLocalScale;
+    //         isFlipped = true;
+    //     }
+    //
+    //     // movendo para a direta flipado. Vai flipar 
+    //     if (isFlipped && pos.x == pointStart.position.x )
+    //     {
+    //         Vector3 newLocalScale = transform.localScale;
+    //         newLocalScale.x *= -1;
+    //         transform.localScale = newLocalScale;
+    //         isFlipped = false;
+    //     }
+    //         
+    // }
+    
+    
     private void Flip()
     {
-        Vector3 pos = transform.position;
         // movendo para a esquerda não flipado. Vai flipar 
-        if (!isFlipped && pos.x == pointEnd.position.x )
+        if (enemyState == EnemyState.Idle)
         {
-            Vector3 newLocalScale = transform.localScale;
-            newLocalScale.x *= -1;
-            transform.localScale = newLocalScale;
-            isFlipped = true;
-        }
+            if (!isFlipped && _rb.velocity.x > 0)
+            {
+                Vector3 newLocalScale = transform.localScale;
+                newLocalScale.x *= -1;
+                transform.localScale = newLocalScale;
+                isFlipped = true;
+            }
 
-        // movendo para a direta flipado. Vai flipar 
-        if (isFlipped && pos.x == pointStart.position.x )
-        {
-            Vector3 newLocalScale = transform.localScale;
-            newLocalScale.x *= -1;
-            transform.localScale = newLocalScale;
-            isFlipped = false;
+            // movendo para a direta flipado. Vai flipar 
+            if (isFlipped && _rb.velocity.x < 0)
+            {
+                Vector3 newLocalScale = transform.localScale;
+                newLocalScale.x *= -1;
+                transform.localScale = newLocalScale;
+                isFlipped = false;
+            }
         }
-            
     }
 }
 
@@ -72,5 +148,6 @@ public enum SimpleEnemyMovements
 public enum EnemyState
 {
     Idle,
-    Damaged
+    Damaged,
+    Aggro
 }
