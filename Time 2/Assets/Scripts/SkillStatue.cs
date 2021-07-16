4a32e61f0e47031a28996dbd21b204723120e252
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2021-07-16 14:42:20 -0300

    Coloca textos das estátuas de habilidade

--- Time 2/Assets/Scripts/SkillStatue.cs
+++ Time 2/Assets/Scripts/SkillStatue.cs
@@ -9,15 +9,39 @@ public class SkillStatue : MonoBehaviour
     public PlayerSkill skill;
     public ParticleSystem idleParticle;
     [HideInInspector]public Text helpDescription;
+    public List<LanguageComponent> SkillTexts;
+    public GameObject Player;
+    private PlayerStatus _playerStatus;
     // Start is called before the first frame update
     void Start()
     {
+        _playerStatus = Player.GetComponent<PlayerStatus>();
         helpDescription = gameObject.GetComponent<Text>();
+        SetText(_playerStatus.controlValue);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetText(_playerStatus.controlValue);
+    }
+
+    public void SetText(int controlValue)
+    {
+        switch (controlValue)
+        {
+            case 0://teclado1
+                helpDescription.text = SkillTexts[0].rightText;
+                break;
+            case 1://teclado2
+                helpDescription.text = SkillTexts[1].rightText;
+                break;
+            case 2://xbox
+                helpDescription.text = SkillTexts[2].rightText;
+                break;
+            case 3://ps
+                helpDescription.text = SkillTexts[3].rightText;
+                break;
+        }
     }
 }
